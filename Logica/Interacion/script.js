7a3ca7baaa86eb2e5edff0c5d3df2f90ae264141
Erick <wojtekvasquez@gmail.com>
2025-04-25 20:38:01 -0400

    aprendiedo _

--- Logica/Interacion/script.js
+++ Logica/Interacion/script.js
@@ -1,3 +1,16 @@
-function Hola(){
-    document.writeln("hola mundo");
+let Valor= 0;
+function Incrementar(){
+    Valor++;
+    document.getElementById("Contador").textContent=Valor;
 }
+
+function mostrarDatos() {
+    // Obtener valores de los campos
+    const nombre = document.getElementById("nombre").value;
+    const edad = document.getElementById("edad").value;
+    const correo = document.getElementById("correo").value;
+
+    // Mostrar datos en el label
+    const resultado = document.getElementById("resultado");
+    resultado.textContent = `Hola ${nombre}, tienes ${edad} años y tu correo es ${correo}.`;
+  }
