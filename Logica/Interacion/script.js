b857c4fb88b4cf5174430c25f179c3e49f5c477e
Erick <wojtekvasquez@gmail.com>
2025-04-25 20:54:42 -0400

    aprendiedo _1

--- Logica/Interacion/script.js
+++ Logica/Interacion/script.js
@@ -14,3 +14,27 @@ function mostrarDatos() {
     const resultado = document.getElementById("resultado");
     resultado.textContent = `Hola ${nombre}, tienes ${edad} años y tu correo es ${correo}.`;
   }
+
+  function obtenerUsuarios() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())     // Convertimos la respuesta a JSON
+      .then(data => {
+        const contenedor = document.getElementById('usuarios');
+        contenedor.innerHTML = '';           // Limpiar resultados anteriores
+
+        data.forEach(usuario => {
+          const div = document.createElement('div');
+          div.className = 'usuario';
+          div.innerHTML = `
+            <strong>Nombre:</strong> ${usuario.name}<br>
+            <strong>Email:</strong> ${usuario.email}<br>
+            <strong>Ciudad:</strong> ${usuario.address.city}<br>
+            <strong>Zipcode:</strong>${usuario.address.zipcode}
+          `;
+          contenedor.appendChild(div);
+        });
+      })
+      .catch(error => {
+        console.error('Hubo un error al obtener los usuarios:', error);
+      });
+  }
